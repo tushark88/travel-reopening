d7fa037e03a21a57bd4ba1e55de7d3c40919425b
Yuri Kovalov <yuri@yurikoval.com>
2020-06-01 22:38:41 +0800

    Use vuex for storing country selection

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -1,28 +1,31 @@
 <template>
   <div class="home">
     <multiselect
-      v-model="country"
       label="name"
-      :options="countryOptions">
+      :value="country"
+      :options="countryOptions"
+      @input="updateCountryAction">
     </multiselect>
   </div>
 </template>
 
 <script>
 import Multiselect from 'vue-multiselect';
-import CountryOptions from '@/constants/countries';
+import { mapState, mapActions } from 'vuex';
 
 export default {
   name: 'CountrySelect',
   components: { Multiselect },
-  data() {
-    return {
-      country: null,
-      countryOptions: CountryOptions,
-    };
-  },
   watch: {
-    country(val) { this.$router.push(`/${val.code}`); },
+    country(val) {
+      this.$router.push(`/${val.code}`);
+    },
+  },
+  computed: mapState({
+    ...mapState(['country', 'countryOptions']),
+  }),
+  methods: {
+    ...mapActions(['updateCountryAction']),
   },
 };
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,23 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import CountryOptions from '@/constants/countries';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    country: null,
+    countryOptions: CountryOptions,
   },
   mutations: {
+    updateCountry(state, country) {
+      state.country = country;
+    },
   },
   actions: {
+    updateCountryAction({ commit }, country) {
+      commit('updateCountry', country);
+    },
   },
   modules: {
   },
