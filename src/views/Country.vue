2ee2664a411111c20e4ae9ff12d15964c5b8b0e4
Yuri Kovalov <yuri@yurikoval.com>
2020-06-01 14:07:03 +0800

    Get country name from country list in Country view

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="country">
-    <h1>We list country-based details here about {{$route.params.country}}</h1>
+    <h1>We list country-based details here about {{countryName}}</h1>
   </div>
 </template>
+
+<script>
+import CountryOptions from '@/constants/countries';
+
+export default {
+  name: 'Country',
+  data() {
+    return {
+      country: null,
+      countryOptions: CountryOptions,
+    };
+  },
+  computed: {
+    currentCoutry() {
+      return CountryOptions.find(({ code }) => code === this.$route.params.country);
+    },
+    countryName() {
+      return this.currentCoutry.name;
+    },
+  },
+};
+</script>
