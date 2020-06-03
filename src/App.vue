c843ab98e2fffd370aa7d1061fa6cc181082069b
Mirha Masala <mirha.masala@me.com>
2020-06-03 11:34:14 +0800

    Create site nav component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link :to="{name: 'Country', params: { country: 'JP' }}">Japan</router-link> |
-      <router-link :to="{name: 'Country', params: { country: 'SG' }}">Singapore</router-link> |
-      <router-link :to="{name: 'Country', params: { country: 'UA' }}">Ukraine</router-link> |
-    </div>
+    <SiteNav/>
     <router-view/>
   </div>
 </template>
+
+<script>
+import SiteNav from '@/components/SiteNav.vue';
+
+export default {
+  name: 'App',
+  components: { SiteNav },
+};
+</script>
