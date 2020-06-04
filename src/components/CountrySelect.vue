78256a084db3fe8088bc8391697f22d5bde7684c
Mirha Masala <mirha.masala@me.com>
2020-06-04 11:23:47 +0800

    Style multiselect with our brand colors

--- src/components/CountrySelect.vue
+++ src/components/CountrySelect.vue
@@ -32,3 +32,17 @@ export default {
   },
 };
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
+
+<style>
+.multiselect__option--highlight,
+.multiselect__option--highlight:after {
+  background-color: #1c355e;
+}
+
+.multiselect__option--selected.multiselect__option--highlight,
+.multiselect__option--selected.multiselect__option--highlight:after {
+  background-color: #d50037;
+}
+</style>
