cd3685baa909adfdb236fce4f01fefe0028c08f1
Mirha Masala <mirha.masala@me.com>
2020-06-04 13:49:32 +0800

    Change map colors to brand colors

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="country">
-    <WorldMap/>
+    <div class="bg-tertiary-light">
+      <div class="container mx-auto">
+        <WorldMap/>
+      </div>
+    </div>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto py-8">
         <CountrySelect/>
