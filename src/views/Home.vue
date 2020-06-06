9aaa1e0085ed285f7bbf66a9486e4e76180148a5
Yuri Kovalov <yuri@yurikoval.com>
2020-06-06 16:42:35 +0800

    Add button to switch travel context

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,6 +8,7 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto py-8">
         <CountrySelect/>
+        <TravelDirectionToggle/>
       </div>
     </div>
   </div>
@@ -15,10 +16,11 @@
 
 <script>
 import CountrySelect from '@/components/CountrySelect.vue';
+import TravelDirectionToggle from '@/components/TravelDirectionToggle.vue';
 import WorldMap from '@/components/WorldMap.vue';
 
 export default {
   name: 'Home',
-  components: { CountrySelect, WorldMap },
+  components: { CountrySelect, TravelDirectionToggle, WorldMap },
 };
 </script>
