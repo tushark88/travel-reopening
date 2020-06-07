413845075148fa2a81fa7b11aff3d8ad08271926
Yuri Kovalov <yuri@yurikoval.com>
2020-06-07 12:07:54 +0800

    Prerender countries list

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,12 @@
+const countries = require('./src/constants/countries.js');
+let renderRoutes = countries.map(function(c) { return "/" + c.code; });
+renderRoutes.push('/about', '/');
+
 module.exports = {
   pluginOptions: {
     prerenderSpa: {
       registry: undefined,
-      renderRoutes: ['/'],
+      renderRoutes: renderRoutes,
       useRenderEvent: true,
       headless: true,
       onlyProduction: true,
