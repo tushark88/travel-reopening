627957c32f1d38934bc03807b31413100923fa29
Mirha Masala <mirha.masala@me.com>
2020-06-09 15:29:30 +0800

    Add footer to app

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <SiteNav/>
     <router-view/>
+    <SiteFooter/>
   </div>
 </template>
 
 <script>
 import SiteNav from '@/components/SiteNav.vue';
+import SiteFooter from '@/components/SiteFooter.vue';
 
 export default {
   name: 'App',
-  components: { SiteNav },
+  components: { SiteNav, SiteFooter },
 };
 </script>
