2ce160403bdfb228ec43e1c8e03cbd80905c36fc
Mirha Masala <mirha.masala@me.com>
2020-06-09 15:29:24 +0800

    Create footer component

---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable max-len -->
+<template>
+  <div class="border-t border-gray-200 pt-8">
+    <p class="text-gray-700 text-center">
+      Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SiteFooter',
+};
+</script>
