0db3e78c8d83cb7a467a88650ef15ec5b2a93be7
Mirha Masala <mirha.masala@me.com>
2020-06-09 15:46:04 +0800

    Change font color nav links

--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -2,8 +2,8 @@
 <template>
   <nav class="bg-white shadow">
     <div class="flex items-center h-12 px-4 sm:px-6 lg:px-8">
-      <router-link class="inline-flex px-2 text-gray-900 font-medium no-underline hover:opacity-100" :to="{ name: 'Home'}">Home</router-link>
-      <router-link class="inline-flex px-2 text-gray-900 font-medium no-underline hover:opacity-100" :to="{ name: 'About'}">About</router-link>
+      <router-link class="inline-flex px-2 text-gray-800 font-medium no-underline hover:opacity-100" :to="{ name: 'Home'}">Home</router-link>
+      <router-link class="inline-flex px-2 text-gray-800 font-medium no-underline hover:opacity-100" :to="{ name: 'About'}">About</router-link>
     </div>
   </nav>
 </template>
