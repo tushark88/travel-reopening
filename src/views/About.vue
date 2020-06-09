cf1d70ab34148249c7811a0d380cee52a0ab674a
Mirha Masala <mirha.masala@me.com>
2020-06-09 14:51:00 +0800

    Add container

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
+    <div class="max-w-3xl mx-auto">
+      <h1>About</h1>
+    </div>
   </div>
 </template>
