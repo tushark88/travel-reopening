b997c0590ddb7718207449564cc45aab0c6d6a07
Mirha Masala <mirha.masala@me.com>
2020-06-09 19:45:05 +0800

    Create toggler

--- src/components/TravelDirectionToggle.vue
+++ src/components/TravelDirectionToggle.vue
@@ -1,9 +1,20 @@
+<!-- eslint-disable max-len -->
 <template>
-  <button
-    v-on:click="toggleContextAction"
-    class="btn bg-primary rounded text-white px-4 py-2 hover:bg-primary-light">
-    {{buttonLabel}}
-  </button>
+  <div @click="toggleContextAction" class="relative border rounded-full">
+    <div
+      :class="isOn ? 'slider--right' : 'slider--left'"
+      class="slider absolute z-10 w-2/4 overflow-hidden bg-tertiary rounded-full transition-all duration-300 ease-linear"></div>
+    <div class="relative z-20 flex">
+      <div
+        :class="isOn ? 'text-gray-900' : 'text-white'"
+        class="w-20 px-3 py-2 text-sm text-center uppercase tracking-wide focus:outline-none">To
+      </div>
+      <div
+        :class="isOn ? 'text-white' : 'text-gray-900'"
+        class="w-20 px-3 py-2 text-sm text-center uppercase tracking-wide focus:outline-none">From
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -13,11 +24,8 @@ export default {
   name: 'CountrySelect',
   computed: mapState({
     ...mapState(['travelContext']),
-    buttonLabel() {
-      if (this.travelContext === 'inbound') {
-        return 'switch to Outbound';
-      }
-      return 'switch to Inbound';
+    isOn() {
+      return this.travelContext === 'inbound';
     },
   }),
   methods: {
@@ -25,3 +33,12 @@ export default {
   },
 };
 </script>
+
+<style>
+  .slider {
+    top: 0;
+    bottom: 0;
+  }
+  .slider--left { left: 0; }
+  .slider--right { left: 50%; }
+</style>
