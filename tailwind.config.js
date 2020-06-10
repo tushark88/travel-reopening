72cd155a456a20df98def80b7c7314006ca72b5e
Mirha Masala <mirha.masala@me.com>
2020-06-10 14:03:50 +0800

    Add position value

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
     fontFamily: {
       body: ['Gilroy', 'sans-serif'],
     },
-
+    inset: {
+      '1/2': '50%',
+    },
     extend: {
       colors: {
         primary: '#1c355e',
