144007c8654f2cbbc20a57b281c6ee55e3952a1b
Mirha Masala <mirha.masala@me.com>
2020-06-11 12:46:15 +0800

    Add 'notice a mistake' link and style

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,10 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div class="border-t border-gray-200 pt-8">
-    <p class="text-gray-700 text-center">
+  <div class="flex justify-between p-4 sm:px-6 lg:px-8 border-t border-gray-200">
+    <p class="flex-shrink-0 mr-2 mb-0 text-gray-700 text-sm">
+      Notice a mistake? <a class="inline-flex no-underline" href="mailto:reopen@tourhero.com">Let us know</a>
+    </p>
+    <p class="flex-shrink-0 mb-0 text-gray-700 text-sm">
       Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
     </p>
   </div>
