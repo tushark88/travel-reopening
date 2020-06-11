c0b67ef67c7d81685d88487f37948989685a0fe1
Mirha Masala <mirha.masala@me.com>
2020-06-11 14:09:12 +0800

    Make container width larger

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
-    <div class="max-w-3xl mx-auto">
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-4xl mx-auto py-6">
       <h1>About</h1>
     </div>
   </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div class="max-w-7xl mx-auto px-4 py-4 sm:px-6 lg:px-8">
-    <div class="max-w-3xl mx-auto">
+  <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div class="max-w-4xl mx-auto py-6">
       <h1>The Ultimate Guide to COVID–19 Travel Restriction</h1>
       <p class="w-8/12 m-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit deleniti ab cupiditate necessitatibus autem, error obcaecati possimus.</p>
       <div class="py-4">
