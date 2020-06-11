5db5b0ced7b823cf48b8f1554e9e6982b78fa3d8
Mirha Masala <mirha.masala@me.com>
2020-06-11 13:58:56 +0800

    Add logo

--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -1,10 +1,17 @@
 <!-- eslint-disable max-len -->
 <template>
   <nav class="bg-white shadow">
-    <div class="flex items-center h-12 px-4 sm:px-6 lg:px-8">
-      <router-link class="inline-flex px-2 text-gray-800 font-medium no-underline hover:opacity-100" :to="{ name: 'Home'}">Home</router-link>
-      <router-link class="inline-flex px-2 text-gray-800 font-medium no-underline hover:opacity-100" :to="{ name: 'About'}">About</router-link>
-      <a class="inline-flex px-2 text-gray-800 font-medium no-underline hover:opacity-100" href="https://github.com/tourhero/travel-reopening">Contribute</a>
+    <div class="flex h-12 px-4 sm:px-6 lg:px-8">
+      <div class="flex-shrink-0 flex items-center">
+        <router-link class="pr-1 pt-1 no-underline hover:opacity-100" :to="{ name: 'Home'}">
+          <img class="block h-8 w-auto" src="../assets/images/site-logo.png">
+        </router-link>
+      </div>
+      <div class="flex">
+        <router-link class="ml-3 sm:ml-4 inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-700 no-underline hover:text-gray-900" :to="{ name: 'About'}">About</router-link>
+        <a class="ml-3 sm:ml-4 inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-700 no-underline hover:text-gray-900" href="https://github.com/tourhero/travel-reopening">Contribute</a>
+        <a class="ml-3 sm:ml-4 inline-flex items-center px-1 pt-1 text-sm font-medium text-gray-700 no-underline hover:text-gray-900" href="#">Subscribe</a>
+      </div>
     </div>
   </nav>
 </template>
