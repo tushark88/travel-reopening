a5488d61e53bfda58b5497070db765b2ee417dd9
Jann Wong <jannwong@protonmail.com>
2020-06-12 23:41:04 +0800

    Fix email to contact us

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="flex justify-between p-4 sm:px-6 lg:px-8 mt-6 md:mt-8 border-t border-gray-200">
     <p class="flex-shrink-0 mr-2 mb-0 text-gray-700 text-sm">
-      Notice a mistake? <a class="inline-flex no-underline" href="mailto:reopen@tourhero.com">Let us know</a>
+      Notice a mistake? <a class="inline-flex no-underline" href="mailto:travel-map@tourhero.com">Let us know</a>
     </p>
     <p class="flex-shrink-0 mb-0 text-gray-700 text-sm">
       Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
