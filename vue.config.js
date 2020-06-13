d12a40d0b566d48a49368a5768812512daf41463
Yuri Kovalov <yuri@yurikoval.com>
2020-06-13 16:19:24 +0800

    Remove vue-cli-plugin-prerender-spa plugin

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,2 @@
-const countries = require('./src/constants/countries.js');
-
-const renderRoutes = countries.map((c) => `/${c.code}`);
-renderRoutes.push('/about', '/');
-
 module.exports = {
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes,
-      useRenderEvent: true,
-      headless: true,
-      onlyProduction: true,
-    },
-  },
 };
