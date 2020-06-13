705068a085d67a40baf6d2843082ed350da6a814
Yuri Kovalov <yuri@yurikoval.com>
2020-06-13 14:17:13 +0800

    Extract shared title, map, menu into own component

--- src/views/Country.vue
+++ src/views/Country.vue
@@ -1,34 +1,12 @@
 <!-- eslint-disable max-len -->
 <template>
   <div>
+    <TitleMapSelect/>
     <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="max-w-4xl mx-auto pt-6 pb-4 md:py-6">
-        <h1>The Ultimate Guide to COVID–19 Travel Restrictions</h1>
-        <p class="w-full md:w-8/12 m-0">Borders are slowly reopening as we battle COVID–19. Here you will find the latest information for travel restrictions and guidelines. Travel safe!</p>
-      </div>
-    </div>
-    <div class="bg-warm-gray-50">
-      <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-4xl mx-auto py-4 md:py-6">
-          <WorldMap/>
-        </div>
-      </div>
-    </div>
-    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="max-w-4xl mx-auto py-6">
-        <div class="flex items-center py-4 md:py-6">
-          <div class="mr-2">
-            <TravelDirectionToggle/>
-          </div>
-          <CountrySelect/>
-        </div>
-        <div class="bg-white overflow-hidden shadow rounded-md">
-          <div class="px-4 py-5 sm:p-6">
-            <h2>{{country.name}} COVID-19 Travel Update</h2>
-            <p>Follow news and updates of current travel restrictions and reopening timelines for {{country.name}}.</p>
-            <CountryBody :content="this.content"></CountryBody>
-          </div>
-        </div>
+      <div class="bg-white overflow-hidden shadow rounded-md max-w-4xl mx-auto px-4 py-5 pt-6 pb-4 md:py-6">
+        <h2>{{country.name}} COVID-19 Travel Update</h2>
+        <p>Follow news and updates of current travel restrictions and reopening timelines for {{country.name}}.</p>
+        <CountryBody :content="this.content"></CountryBody>
       </div>
     </div>
   </div>
@@ -37,18 +15,14 @@
 <script>
 import axios from 'axios';
 import CountryBody from '@/components/CountryBody.vue';
-import CountrySelect from '@/components/CountrySelect.vue';
-import TravelDirectionToggle from '@/components/TravelDirectionToggle.vue';
+import TitleMapSelect from '@/components/TitleMapSelect.vue';
 import { mapActions, mapGetters, mapState } from 'vuex';
-import WorldMap from '@/components/WorldMap.vue';
 
 export default {
   name: 'Country',
   components: {
     CountryBody,
-    CountrySelect,
-    TravelDirectionToggle,
-    WorldMap,
+    TitleMapSelect,
   },
   data() {
     return {
