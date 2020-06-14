599d795480b9e2da38009719118ff08da78523ec
Yuri Kovalov <yuri@yurikoval.com>
2020-06-14 13:55:24 +0800

    Render reopening component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <TitleMapSelect/>
+  <div>
+    <TitleMapSelect/>
+    <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 my-8">
+      <div class="bg-white overflow-hidden shadow
+        rounded-md max-w-4xl mx-auto px-4 py-5 pt-6 pb-4 md:py-6">
+        <Reopening/>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Reopening from '@/components/Reopening.vue';
 import TitleMapSelect from '@/components/TitleMapSelect.vue';
 
 export default {
   name: 'Home',
-  components: { TitleMapSelect },
+  components: { Reopening, TitleMapSelect },
   mounted() {
     this.$nextTick(() => document.dispatchEvent(new Event('render-completed')));
   },
