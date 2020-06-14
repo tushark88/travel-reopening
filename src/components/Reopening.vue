160ddf85d59c8883cc09ae04c7bc59a09049cab5
Yuri Kovalov <yuri@yurikoval.com>
2020-06-14 13:43:36 +0800

    Add reopening component

---
+++ src/components/Reopening.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h4>Countries Reopening</h4>
+    <ul>
+      <li v-for="reopening in reopenings" :key="reopening.content">
+        <span>{{reopening.date}}:</span>
+        <vue-markdown
+          :source="reopening.content"
+          :anchorAttributes='anchorAttributes'></vue-markdown>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { futureReopenings } from '@/constants/travel';
+import VueMarkdown from 'vue-markdown';
+
+export default {
+  name: 'Reopening',
+  components: {
+    'vue-markdown': VueMarkdown,
+  },
+  data() {
+    return {
+      anchorAttributes: { target: '_blank', rel: 'nofollow' },
+      reopenings: futureReopenings,
+    };
+  },
+};
+</script>
