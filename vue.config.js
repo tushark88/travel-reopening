450955bdfb6eb27b9622a200c86b9265f0026ef2
Yuri Kovalov <yuri@yurikoval.com>
2020-06-15 15:55:14 +0800

    Build production with subdir

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,18 @@ const path = require('path');
 const PrerenderSPAPlugin = require('prerender-spa-plugin');
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
+const basePath = process.env.NODE_ENV === 'production' ?
+  '/en/ultimate-guide-covid19-travel-restrictions-reopening/' : '';
+const buildDir = 'dist';
+
 const countries = require('./src/constants/countries.js');
-const routes = countries.map((c) => `/${c.code}.html`);
-routes.push('/about.html', '/');
+const routes = countries.map((c) => `${basePath}${c.code}.html`);
+routes.push(`${basePath}about.html`, basePath);
 
 const productionPlugins = [
   new PrerenderSPAPlugin({
-    staticDir: path.join(__dirname, 'dist'),
+    staticDir: path.join(__dirname, buildDir),
+    indexPath: path.join(__dirname, `${buildDir}${basePath}index.html`),
     routes,
     postProcess (renderedRoute) {
       // Ignore any redirects.
@@ -16,7 +21,7 @@ const productionPlugins = [
       // Remove /index.html from the output path if the dir name ends with a .html file extension.
       // For example: /dist/dir/special.html/index.html -> /dist/dir/special.html
       if (renderedRoute.route.endsWith('.html')) {
-        renderedRoute.outputPath = path.join(__dirname, 'dist', renderedRoute.route)
+        renderedRoute.outputPath = path.join(__dirname, buildDir, renderedRoute.route)
       }
 
       return renderedRoute
@@ -36,6 +41,8 @@ const productionPlugins = [
 ];
 
 module.exports = {
+  publicPath: basePath,
+  outputDir: `${buildDir}${basePath}`,
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(...productionPlugins);
