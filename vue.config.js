722be1547b3e6d6b98ddc660ebed0d736d56378f
Yuri Kovalov <yuri@yurikoval.com>
2020-06-15 16:20:37 +0800

    Reduce concurrent builds

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ const productionPlugins = [
       sortAttributes: true
     },
     renderer: new Renderer({
-      maxConcurrentRoutes: 50,
+      maxConcurrentRoutes: 30,
       renderAfterDocumentEvent: 'render-completed',
     }),
   })
