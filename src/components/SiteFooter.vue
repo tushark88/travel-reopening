8401089557cf98c6d315c41baaff0a1140ad16b7
Mirha Masala <mirha.masala@me.com>
2020-06-16 13:52:50 +0800

    Add heart travel svg

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,12 +1,17 @@
-<!-- eslint-disable max-len -->
 <template>
   <div class="p-4 sm:px-6 lg:px-8 mt-6 md:mt-8 border-t border-gray-200">
-    <div class="flex flex-col items-center sm:flex-row sm:justify-between mb-1">
+    <div class="flex flex-col items-starts sm:flex-row sm:items-center sm:justify-between mb-1">
       <p class="sm:mb-0 text-gray-700 text-sm">
-        Notice a mistake? <a class="inline-flex no-underline" href="mailto:travel-map@tourhero.com">Let us know</a>
+        Notice a mistake?
+        <a class="inline-flex no-underline" href="mailto:travel-map@tourhero.com">Let us know</a>
       </p>
-      <p class="mb-0 text-gray-700 text-sm">
-        Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
+      <p class="sm:mb-0 text-gray-700 text-sm">
+        Made with
+        <svg class="inline-flex items-center h-6 w-6 sm:h-8 sm:h-8 text-secondary fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M84.161 21.479c-3.212-3.247-8.166-6.082-15.185-6.014-12.146.869-16.146 8.494-19.1 14.728-2.921-6.359-7.796-14.859-20.446-14.728-10.358.663-16.548 6.979-18.793 14.884-2.737 9.639.781 20.264 5.113 26.311 4.472 6.24 10.976 11.425 16.688 16.391 6.063 5.27 9.893 7.284 17.591 13.637 7.635-7.021 19.635-14.521 29.616-24.912 4.526-4.907 8.479-10.01 9.771-18.494 1.377-8.981-.198-16.694-5.255-21.803zM80.188 40.83l-.013.027c-.135.307-.41.392-1.702.065l-7.314-1.975-.112 5.362 2.128 2.223a.357.357 0 01.099.25v1.005c0 .298-.318.438-.54.308l-3.132-1.213-3.113 1.213c-.223.13-.541-.01-.541-.308v-1.005c0-.095.036-.184.099-.25l2.208-2.306-.107-5.202-7.127 1.898c-1.335.371-1.569.242-1.703-.065l-.013-.027c-.382-.872-.084-1.901.665-2.3l8.107-4.297-.087-5.29c0-.95.701-1.722 1.563-1.722h.198c.863 0 1.563.772 1.563 1.722l-.086 5.242 8.297 4.345c.748.399 1.046 1.428.663 2.3z"/></svg>
+        by
+        <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
+      </p>
+      <p class="mb-0 text-gray-700 text-sm"><a class="inline-flex no-underline" href="https://www.tourhero.com/en/privacy-cookie-policy">Privacy Policy</a>
       </p>
     </div>
   </div>
