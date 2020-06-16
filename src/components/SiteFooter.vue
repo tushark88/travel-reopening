edcd5a8259cd121cc9f8bf0c4b118862e2433b1a
Mirha Masala <mirha.masala@me.com>
2020-06-16 11:49:51 +0800

    Break footer to column on smaller devices

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,12 +1,14 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div class="flex justify-between p-4 sm:px-6 lg:px-8 mt-6 md:mt-8 border-t border-gray-200">
-    <p class="flex-shrink-0 mr-2 mb-0 text-gray-700 text-sm">
-      Notice a mistake? <a class="inline-flex no-underline" href="mailto:travel-map@tourhero.com">Let us know</a>
-    </p>
-    <p class="flex-shrink-0 mb-0 text-gray-700 text-sm">
-      Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
-    </p>
+  <div class="p-4 sm:px-6 lg:px-8 mt-6 md:mt-8 border-t border-gray-200">
+    <div class="flex flex-col items-center sm:flex-row sm:justify-between mb-1">
+      <p class="sm:mb-0 text-gray-700 text-sm">
+        Notice a mistake? <a class="inline-flex no-underline" href="mailto:travel-map@tourhero.com">Let us know</a>
+      </p>
+      <p class="mb-0 text-gray-700 text-sm">
+        Made with ❤️ by <a class="inline-flex no-underline" href="https://www.tourhero.com/">TourHero</a>
+      </p>
+    </div>
   </div>
 </template>
 
