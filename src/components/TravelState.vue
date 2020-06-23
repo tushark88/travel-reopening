20b503e1d6dc65c5e1e04e4bcea6be3da7f4b17e
mirhamasala <mirha.masala@me.com>
2020-06-23 16:04:33 +0800

    Fix order classes declaration

--- src/components/TravelState.vue
+++ src/components/TravelState.vue
@@ -5,19 +5,19 @@
       <span>Domestic</span>
       <span
         :class="travelStateLabelColor(state.domestic)"
-        class="flex items-center justify-center px-2.5 py-0.5 rounded-md text-center">
+        class="flex items-center justify-center px-2.5 py-0.5 text-center rounded-md">
         {{ travelStateLabel(state.domestic) }}
       </span>
       <span>Entry</span>
       <span
         :class="travelStateLabelColor(state.inbound)"
-        class="flex items-center justify-center px-2.5 py-0.5 rounded-md text-center">
+        class="flex items-center justify-center px-2.5 py-0.5 text-center rounded-md">
         {{ travelStateLabel(state.inbound) }}
       </span>
       <span>Exit</span>
       <span
         :class="travelStateLabelColor(state.outbound)"
-        class="flex items-center justify-center px-2.5 py-0.5 rounded-md text-center">
+        class="flex items-center justify-center px-2.5 py-0.5 text-center rounded-md">
         {{ travelStateLabel(state.outbound) }}
       </span>
     </div>
