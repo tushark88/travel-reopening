72a16832d117c35a6f6e910c050a56f3d294d5cc
Yuri Kovalov <yuri@yurikoval.com>
2020-07-02 14:40:59 +0800

    Search country based on searchKey

--- src/constants/countries.js
+++ src/constants/countries.js
@@ -14,12 +14,17 @@ const toSlug = (name) => {
 
 const countries = countriesJson.map((c) => {
   const name = c['CLDR display name'];
+  const searchKey = [
+    name,
+  ].join(',');
+
   return {
     id: c['ISO3166-1-numeric'],
     code: c['ISO3166-1-Alpha-2'],
     name,
     alpha3: c['ISO3166-1-Alpha-3'],
     slug: toSlug(name),
+    searchKey: removeDiacritics(searchKey),
   };
 }).filter(({ name }) => !!name);
 
