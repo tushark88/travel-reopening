20a140f36bf64247ff828f9fec97e2b05e93def2
mirhamasala <mirha.masala@me.com>
2020-07-13 18:00:40 +0800

    Add ShareNetwork component to page

--- src/components/ShareNetwork.vue
+++ src/components/ShareNetwork.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="flex items-start">
-    <ShareNetwork v-for="network in networks"
+    <ShareNetwork
+      v-for="network in networks"
       :network="network.network"
       :key="network.network"
-      :url="sharing.url"
-      :title="sharing.title"
-      :description="sharing.description"
-      :quote="sharing.quote"
-      :hashtags="sharing.hashtags"
-      :twitterUser="sharing.twitterUser"
+      url="https://www.tourhero.com/en/ultimate-guide-covid19-travel-restrictions-reopening/"
+      title="The Ultimate Guide to COVID-19 Travel Restrictions"
+      description="A free and open-source project by TourHero.
+        Find the latest information for travel restrictions around
+        the world during this COVID-19 pandemic.
+        Travel safe!"
+      quote="Get the latest travel reopening information"
+      hashtags="touherotravel"
+      twitterUser="touherotravel"
     >
-      <button class="h-6 w-6">
-        <img
-          :src="require(`@/assets/images/social_${network.icon}.svg`)"
-          class="mr-1">
-      </button>
+      <img :src="require(`@/assets/images/social_${network.icon}.svg`)"
+        class="h-6 w-6 mr-2 sm:mr-0 sm:ml-3 cursor-pointer" />
     </ShareNetwork>
   </div>
 </template>
@@ -27,20 +28,12 @@ Vue.use(VueSocialSharing);
 export default {
   data() {
     return {
-      sharing: {
-        url: 'https://www.tourhero.com/en/ultimate-guide-covid19-travel-restrictions-reopening/',
-        title: 'The Ultimate Guide to COVID-19 Travel Restrictions',
-        description: 'A free and open-source project by TourHero. Find the latest information for travel restrictions around the world during this COVID-19 pandemic.Travel safe!',
-        quote: 'Get the latest travel reopening information',
-        hashtags: 'tourherotravel',
-        twitterUser: 'tourherotravel',
-      },
       networks: [
         { network: 'facebook', icon: 'facebook' },
-        // { network: 'linkedin', icon: 'linkedin' },
-        // { network: 'telegram', icon: 'telegram' },
         { network: 'twitter', icon: 'twitter' },
-        // { network: 'whatsapp', icon: 'whatsapp' },
+        { network: 'linkedin', icon: 'linkedin' },
+        { network: 'telegram', icon: 'telegram' },
+        { network: 'whatsapp', icon: 'whatsapp' },
       ],
     };
   },
